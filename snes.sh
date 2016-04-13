2423e886c30ef7edbba698318b21d064247721be
Kannagi <kannagichan@gmail.com>
2016-04-13 08:26:25 +0200

    Update snes.sh

--- snes.sh
+++ snes.sh
@@ -3,8 +3,8 @@
 echo '[objects]' > temp
 echo main.obj >> temp
 
-/home/kannagi/Documents/SDK/SNES/bin/wla-65816 -o main.asm main.obj
-/home/kannagi/Documents/SDK/SNES/bin/wlalink -vr temp main.smc
+wla-65816 -o main.asm main.obj
+wlalink -vr temp main.smc
 
 rm main.obj
 rm temp
